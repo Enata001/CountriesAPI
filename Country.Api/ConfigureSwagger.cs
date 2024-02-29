d360a02a53c502cd7622f617c0877dc645097225
Enata <emmanueltag123@gmail.com>
2024-02-29 11:25:54 +0000

    Configured swagger support for versioning

---
+++ Country.Api/ConfigureSwagger.cs
@@ -0,0 +1,38 @@
+﻿using Asp.Versioning.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Country.Api;
+
+public class ConfigureSwagger: IConfigureNamedOptions<SwaggerGenOptions>
+{
+    private readonly IApiVersionDescriptionProvider _apiVersionDescriptionProvider;
+
+    public ConfigureSwagger(IApiVersionDescriptionProvider apiVersionDescriptionProvider)
+    {
+        _apiVersionDescriptionProvider = apiVersionDescriptionProvider;
+    }
+    public void Configure(SwaggerGenOptions options)
+    {
+       Configure(options);
+    }
+
+    public void Configure(string name, SwaggerGenOptions options)
+    {
+        foreach (var item in _apiVersionDescriptionProvider.ApiVersionDescriptions)
+        {
+            options.SwaggerDoc(item.GroupName,CreateVersionInfo(item));
+        }
+    }
+
+    private OpenApiInfo CreateVersionInfo(ApiVersionDescription description)
+    {
+        var info = new OpenApiInfo()
+        {
+            Title = "Your Versioned Api",
+            Version = description.ApiVersion.ToString()
+        };
+        return info;
+    }
+}
--- Country.Api/Program.cs
+++ Country.Api/Program.cs
@@ -1,5 +1,6 @@
 using Asp.Versioning;
-using Microsoft.AspNetCore.Authentication;
+using Asp.Versioning.ApiExplorer;
+using Country.Api;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 
@@ -15,23 +16,39 @@ builder.Services.AddControllers();
 builder.Services.AddApiVersioning(options =>
 {
     options.AssumeDefaultVersionWhenUnspecified = true;
-    options.DefaultApiVersion = ApiVersion.Default;
+    options.DefaultApiVersion = new ApiVersion(1,0);
+    options.ReportApiVersions = true;
 
+}).AddApiExplorer(options =>
+{
+    options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
 });
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.ConfigureOptions<ConfigureSwagger>();
 
 var app = builder.Build();
 
+var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 // Configure the HTTP request pipeline.
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(options =>
+    {
+        foreach (var description in provider.ApiVersionDescriptions)
+        {
+            options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+            
+        }
+    });
+    
 
+}
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
